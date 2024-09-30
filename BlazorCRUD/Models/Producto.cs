2208c863cbf99dc3c131fa01b991acc5358d6608
AleFe2425 <ignacio.fonte@udla.edu.ec>
2024-09-30 03:11:16 -0500

    Comentarios  y Pagina Home

--- BlazorCRUD/Models/Producto.cs
+++ BlazorCRUD/Models/Producto.cs
@@ -2,7 +2,7 @@
 {
     public class Producto
     {
-        public Guid Id { get; set; }
+        public Guid Id { get; set; } //propiedades
         public string Nombre { get; set; }    
         public double Precio { get; set; }
         public int Qty { get; set; }
